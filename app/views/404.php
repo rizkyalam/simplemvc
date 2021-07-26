966fed4297c8c8bd5a2b85fd52be46b1dc35f36f
alam <ralamsya0976@gmail.com>
2021-07-26 21:21:12 +0700

    init app version 1.0.0

--- app/views/404.php
+++ app/views/404.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="<?= public_asset('favicon.ico') ?>">
   <title>404 Not Found</title>
   <link rel="stylesheet" href="<?= public_asset('css/style.css') ?>">
 </head>
